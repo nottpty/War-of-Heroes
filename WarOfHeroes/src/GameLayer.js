e7f70f9731a6968372563a9c12fa783150ca8f56
Not <patinya.y@ku.th>
2015-03-25 14:53:51 +0700

    Clean code

--- WarOfHeroes/src/GameLayer.js
+++ WarOfHeroes/src/GameLayer.js
@@ -1,15 +1,28 @@
 var GameLayer = cc.LayerColor.extend({
 	init: function() {
-		this.background = cc.Sprite.create( "res/images/background2.gif" );
-		this.background.setPosition( new cc.Point( screenWidth / 2, screenHeight / 2) );
-		this.player = new Player();
-		this.player.setPosition( new cc.Point( screenWidth / 2, screenHeight / 8) );
-		this.addChild( this.background );
-		this.addChild( this.player );
+        this.createBackground();
+        this.createPlayer();
+        this.createAddChild();
 		this.addKeyboardHandlers();
 		this.state = GameLayer.STATES.FRONT;
 		this.player.scheduleUpdate();
 	},
+
+    createBackground: function() {
+        this.background = cc.Sprite.create( "res/images/background2.gif" );
+        this.background.setPosition( new cc.Point( screenWidth / 2, screenHeight / 2) );
+    },
+
+    createPlayer: function() {
+        this.player = new Player();
+        this.player.setPosition( new cc.Point( screenWidth / 2, screenHeight / 8) );
+    },
+
+    createAddChild: function() {
+        this.addChild( this.background );
+        this.addChild( this.player );
+    },
+
 	addKeyboardHandlers: function(){
         var self = this;
         cc.eventManager.addListener({
@@ -23,12 +36,17 @@ var GameLayer = cc.LayerColor.extend({
         }, this);
     },
 
-             onKeyDown: function( keyCode, event ) {
-        this.player.switchDirection  (keyCode);
+    onKeyDown: function( keyCode, event ) {
+        console.log('PRESS :' + keyCode.toString());
+        if(keyCode == GameLayer.ARROWDIR.LEFT || keyCode == GameLayer.ARROWDIR.RIGHT ){
+            this.player.switchDirection(keyCode);
+            this.player.checkPlayers();
+        }
     },
 
     onKeyUp: function( keyCode, event ) {
-    console.log( 'Up: ' + keyCode.toString() );
+        console.log( 'Up: ' + keyCode.toString() );
+        this.player.stopPlayer();
     }
 });
 
@@ -38,7 +56,7 @@ var StartScene = cc.Scene.extend({
         var layer = new GameLayer();
         layer.init();
         this.addChild( layer );
-    },
+    }
 });
 
 GameLayer.STATES = {
--- WarOfHeroes/src/Player.js
+++ WarOfHeroes/src/Player.js
@@ -5,37 +5,32 @@ var Player = cc.Sprite.extend({
         this.vy = 15;
         this.started = false;
         this.jumped = false;
+        this.checkPlayer = false;
     },
 
     update: function( dt ) {
 
     	if(this.started){
-
-            // if(this.jumped){
-            // var pos = this.getPosition();
-            // this.setPosition( new cc.Point( pos.x, pos.y + this.vy ) );
-            // this.vy += -1;
-            // this.jumped = false;
-            // }
-
-        // else{
-            this.pos = this.getPosition();
-
-            if( this.direction == Player.DIR.RIGHT ){
-            this.rightDirection(this.pos);
-            }
-
-            else if( this.direction == Player.DIR.LEFT ){
-            this.leftDirection(this.pos);
-            }
-    	}
+            if(this.checkPlayer){
+                this.pos = this.getPosition(); 
+                if( this.direction == Player.DIR.RIGHT ){
+                    this.rightDirection(this.pos);
+                }
+                else if( this.direction == Player.DIR.LEFT ){
+                    this.leftDirection(this.pos);
+                }
+    	   }
+        }
     },
 
-    // jump: function() {
-    //     this.jumped = true;
-    //     this.vy = Player.JUMPING_VELOCITY;
-    // },
-    
+    checkPlayers: function() {
+        this.checkPlayer = true;
+    },
+
+    stopPlayer: function() {
+        this.checkPlayer = false;
+    },
+
     start: function() {
         this.started = true;
     },
@@ -44,28 +39,25 @@ var Player = cc.Sprite.extend({
         this.start();
         if ( arrowDirection==GameLayer.ARROWDIR.LEFT ) {
             this.direction = Player.DIR.LEFT;
-            // this.setRotation( 270 );
         } else if(arrowDirection == GameLayer.ARROWDIR.RIGHT){
             this.direction = Player.DIR.RIGHT;
-            // this.setRotation( 90 );
         }
     },
 
     rightDirection: function(pos) {
         if( pos.x < screenWidth ){
             this.setPosition( new cc.Point( pos.x + 5,pos.y) );
-            }else {
-                this.setPosition( new cc.Point( 0 , pos.y));
-            }
+        }else {
+            this.setPosition( new cc.Point( 0 , pos.y));
+        }
     },
 
     leftDirection: function(pos) {
         if( pos.x < 0 ){
-            this.setPosition( new cc.Point( screenWidth , pos.y));
-            
-            }else {
+            this.setPosition( new cc.Point( screenWidth , pos.y));   
+        }else {
                 this.setPosition( new cc.Point( pos.x - 5,pos.y) );
-            }
+        }
     }
     
 });
@@ -74,7 +66,3 @@ Player.DIR = {
     RIGHT: 1,
     LEFT: 2
 };
-
-// Player.G = -1;
-// Player.STARTING_VELOCITY = 15;
-// Player.JUMPING_VELOCITY = 15;
